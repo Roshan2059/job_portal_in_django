3d0de7a456e25efde24b057d80850adf353c1e99
Roshan Panta <roshanpanta2059@gmail.com>
2024-01-07 10:08:19 +0545

    added auth app

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Employer, Job, JobSeeker, JobApplication
 
-admin.site.register(Employer)
-admin.site.register(Job)
-admin.site.register(JobSeeker)
-admin.site.register(JobApplication)
+all_models = [Employer, Job, JobApplication, JobSeeker]
+admin.site.register(all_models)
+
