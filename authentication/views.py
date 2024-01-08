78a6869d00389996d73a17386f7afa026cd04c7c
Roshan Panta <roshanpanta2059@gmail.com>
2024-01-08 07:47:07 +0545

    added some code of register

--- authentication/views.py
+++ authentication/views.py
@@ -1,22 +1,33 @@
+from django import forms
 from home.models import Job, JobApplication, Employer, JobSeeker
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 def signup(request):
     if request.method == 'GET':
         return render(request, 'register.html')
     else:
+        # if forms.is_valid():
         # Get the user info from the form data
-        fn = request.POST['firstname']
-        ln = request.POST['lastname']
-        un = request.POST['username']
-        em = request.POST['email']
-        role = request.POST['role']
-        pw1 = request.POST['password']
-        pw2 = request.POST['confirm_password']
+            fn = request.POST['firstname']
+            ln = request.POST['lastname']
+            un = request.POST['username']
+            # em = request.POST['email']
+            role = request.POST['role']
+            pw = request.POST['password']
+            try:
+                User.objects.create_user(first_name = fn, last_name = ln, username=un, email=em, role=role, password=pw )
+                return redirect('login')
+            except:
+                messages.error(request, "please fill the form properly")
+                return redirect('signup')
+        
+    
 
 def login(request):
     if request.method == "GET":
